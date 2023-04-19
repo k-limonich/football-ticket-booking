24d9c12a1b64ef7a064a46582a66f46f8c8b819a
Aliaksei Klimovich <k.limonich@gmail.com>
2023-04-19 15:13:19 +0300

    Create ServletInitializer and Application runner

---
+++ src/main/java/by/bsuir/ticketbooking/FootballTicketBookingApplication.java
@@ -0,0 +1,13 @@
+package by.bsuir.ticketbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballTicketBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballTicketBookingApplication.class, args);
+	}
+
+}
---
+++ src/main/java/by/bsuir/ticketbooking/ServletInitializer.java
@@ -0,0 +1,13 @@
+package by.bsuir.ticketbooking;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(FootballTicketBookingApplication.class);
+	}
+
+}
