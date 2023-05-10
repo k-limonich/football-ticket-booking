eb22797162e5b4f2f13a5c6f794241b7b0d013de
Aliaksei Klimovich <k.limonich@gmail.com>
2023-05-10 23:35:10 +0300

    Add email not registered exception handling

--- src/main/java/by/bsuir/ticketbooking/controller/ExceptionAdvice.java
+++ src/main/java/by/bsuir/ticketbooking/controller/ExceptionAdvice.java
@@ -3,6 +3,7 @@ package by.bsuir.ticketbooking.controller;
 import by.bsuir.ticketbooking.dto.UserLoginRequest;
 import by.bsuir.ticketbooking.dto.UserRegisterRequest;
 import by.bsuir.ticketbooking.exception.EmailAlreadyRegisteredException;
+import by.bsuir.ticketbooking.exception.EmailNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -18,6 +19,15 @@ public class ExceptionAdvice {
 	public ModelAndView badCredentials() {
 		ModelAndView modelAndView = new ModelAndView("login");
 		modelAndView.addObject("ex", "Некорректный пароль");
+		modelAndView.addObject("loginRequest", UserLoginRequest.builder().build());
+		return modelAndView;
+	}
+
+	@ExceptionHandler
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	public ModelAndView emailNotRegistered(EmailNotFoundException e) {
+		ModelAndView modelAndView = new ModelAndView("login");
+		modelAndView.addObject("ex", e.getMessage());
 		modelAndView.addObject("loginRequest", UserLoginRequest.builder().build());
 		return modelAndView;
 	}
